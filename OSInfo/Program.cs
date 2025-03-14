e61dd6ffb82733f2ecee376d41b2c9bca33ea970
matheus <matios.torrres@gmail.com>
2025-03-14 08:48:19 -0300

    Commit do projeto OSInfo

---
+++ OSInfo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OSInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+        
+        
+    }
+}
